45118d69f3bb6c286aee8be68fa8cc100094be88
rafa321563 <latypo.rafael@gmail.com>
2025-11-22 14:01:04 +0300

    Initial Commit

---
+++ WardrobeAssistance_v1.1/WardrobeAssistance_v1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeAssistance_v1_1App.swift
+//  WardrobeAssistance_v1.1
+//
+//  Created by Рафаэл Латыпов on 22.11.25.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeAssistance_v1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
